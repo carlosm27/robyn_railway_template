f34ade5ac2ce09b2c4236042d8f66ad2363124fe
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-11-19 15:44:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from robyn import Robyn, static_file
-
+from dotenv import load_dotenv
+import os
 
 app = Robyn(__file__)
 
+load_dotenv()
+
+PORT = os.environ.get('PORT')
 
 @app.get("/hello")
 async def h(request):
@@ -14,4 +18,4 @@ async def get_page(request):
     return static_file("./index.html")
 
 
-app.start()    
+app.start(url="0.0.0.0",port=PORT)    

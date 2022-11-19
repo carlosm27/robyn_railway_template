522d5d0fe3659c044154a9ca60d4a45c5939241e
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-11-19 16:20:17 -0400

    Add Robyn instance

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
 
     load_dotenv()
     PORT = os.environ.get("PORT")
-    Robyn.start(url="0.0.0.0", port=PORT)    
+    app = Robyn(__file__)
+    app.start(url="0.0.0.0", port=PORT)    
